8429c1982c664aefb16412ffc2c79e5e6dd68bc5
joao-zanetti <57362590+joao-zanetti@users.noreply.github.com>
2020-01-21 11:36:33 -0300

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+
+namespace SplashScreen
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private ProgressBar _progressBarSplash;
+        private TextView _labelVersionapp;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_splash);
+
+            _progressBarSplash = FindViewById<ProgressBar>(Resource.Id.progressBarSplash);
+            _labelVersionapp = FindViewById<TextView>(Resource.Id.labelVersionapp);
+
+            //Version of App
+            _labelVersionapp.Text = "1.0";
+            _progressBarSplash.Visibility = ViewStates.Visible;
+
+        }
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
